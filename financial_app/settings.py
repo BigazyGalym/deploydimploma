52fbfb6458e83db814c2300b588f937083e34642
gabiproject1 <playstationgabi3@gmail.com>
2026-08-06 15:30:42 +0500

    Remove allauth AccountMiddleware from MIDDLEWARE stack

--- financial_app/settings.py
+++ financial_app/settings.py
@@ -89,7 +89,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'accounts.middleware.UserActivityMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
