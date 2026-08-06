3ca72c664c96f66353bbf88c9208942c68d82150
gabiproject1 <playstationgabi3@gmail.com>
2026-08-06 15:36:29 +0500

    Add post-initialization auto-migration to wsgi.py

--- financial_app/wsgi.py
+++ financial_app/wsgi.py
@@ -13,3 +13,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financial_app.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.db import connection
+    from django.core.management import call_command
+    if 'accounts_user' not in connection.introspection.table_names():
+        call_command('migrate', '--no-input')
+except Exception as _e:
+        print(f"WSGI auto-migrate info: {_e}")
