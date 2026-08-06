409c5778fc5cfd601d0a14b9d30bddb751102261
gabiproject1 <playstationgabi3@gmail.com>
2026-08-06 15:25:01 +0500

    Auto-create database tables on ready() if accounts_user is missing

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        try:
+            from django.db import connection
+            from django.core.management import call_command
+            tables = connection.introspection.table_names()
+            if 'accounts_user' not in tables:
+                call_command('migrate', interactive=False)
+        except Exception:
+            pass
--- financial_app/settings.py
+++ financial_app/settings.py
@@ -147,7 +147,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': Path('/tmp') / 'finance_db.sqlite3' if os.name != 'nt' else BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
